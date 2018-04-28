5c4edab5ecae485ca163f6a357c47e52797bde61
Lijun_Temco <lijun@temcocontrols.com>
2018-04-28 15:28:24 +0800

    Fixed a watchdog bug. 4/28/2018

--- watchdog/watchdog.c
+++ watchdog/watchdog.c
@@ -6,9 +6,9 @@ void watchdog_init(void)
 		/* Enable write access to IWDG_PR and IWDG_RLR registers */ 
 		IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
 		/* IWDG counter clock: 40KHz(LSI) / 4 = 10 KHz */ 
-		IWDG_SetPrescaler(IWDG_Prescaler_4); 
+		IWDG_SetPrescaler(IWDG_Prescaler_64); 
 		/* Set counter reload value to 10000 = 1s */ 
-		IWDG_SetReload(30000); 
+		IWDG_SetReload(4000); 
 		IWDG_ReloadCounter(); // reload the value
 		IWDG_Enable();  			//enable the watchdog
 
